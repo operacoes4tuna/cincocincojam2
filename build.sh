5825f0968db8e4068377ae010a638af4d5c40cb8
Bruno Nascimento <bruno@brunonascimento.com>
2025-05-12 20:11:45 -0300

    corrigindo erro no deploy

--- build.sh
+++ build.sh
@@ -21,9 +21,11 @@ python manage.py collectstatic --no-input
 # Criar schema public se não existir (apenas em produção)
 if [ "$RENDER" = "true" ]; then
     echo "Criando schema public no banco de dados..."
-    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "CREATE SCHEMA IF NOT EXISTS public;"
-    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "GRANT ALL ON SCHEMA public TO $DB_USER;"
-    PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "ALTER DATABASE $DB_NAME SET search_path TO public;"
+    export PGPASSWORD=$DB_PASSWORD
+    psql -d $DB_NAME -h $DB_HOST -U $DB_USER -c "CREATE SCHEMA IF NOT EXISTS public;"
+    psql -d $DB_NAME -h $DB_HOST -U $DB_USER -c "GRANT ALL ON SCHEMA public TO $DB_USER;"
+    psql -d $DB_NAME -h $DB_HOST -U $DB_USER -c "ALTER DATABASE $DB_NAME SET search_path TO public;"
+    unset PGPASSWORD
 fi
 
 # Executar migrações
